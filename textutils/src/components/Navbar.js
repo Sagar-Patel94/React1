d326f9905b8579bc47b38aec6c53b22ca9b502eb
Sagar-Patel94 <s.m.gajera.sg@gmail.com>
2022-09-22 14:29:22 +0530

    22/09/2022 Thursday 02:29 PM

--- textutils/src/components/Navbar.js
+++ textutils/src/components/Navbar.js
@@ -25,7 +25,7 @@ export default function Navbar(props) {
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
-              <Link className="nav-link active" aria-current="page" to="/">
+              <Link className="nav-link" aria-current="page" to="/">
                 Home
               </Link>
             </li>
@@ -35,6 +35,80 @@ export default function Navbar(props) {
               </Link>
             </li>
           </ul>
+          {/* <div className="d-flex">
+            <div
+              className="bg-primary rounded mx-2"
+              onClick={() => {
+                props.toggleMode("primary");
+              }}
+              style={{
+                height: "30px",
+                width: "30px",
+                cursor: "pointer",
+                border: "1px solid black",
+              }}
+            ></div>
+            <div
+              className="bg-danger rounded mx-2"
+              onClick={() => {
+                props.toggleMode("danger");
+              }}
+              style={{
+                height: "30px",
+                width: "30px",
+                cursor: "pointer",
+                border: "1px solid black",
+              }}
+            ></div>
+            <div
+              className="bg-success rounded mx-2"
+              onClick={() => {
+                props.toggleMode("success");
+              }}
+              style={{
+                height: "30px",
+                width: "30px",
+                cursor: "pointer",
+                border: "1px solid black",
+              }}
+            ></div>
+            <div
+              className="bg-warning rounded mx-2"
+              onClick={() => {
+                props.toggleMode("warning");
+              }}
+              style={{
+                height: "30px",
+                width: "30px",
+                cursor: "pointer",
+                border: "1px solid black",
+              }}
+            ></div>
+            <div
+              className="bg-light rounded mx-2"
+              onClick={() => {
+                props.toggleMode("light");
+              }}
+              style={{
+                height: "30px",
+                width: "30px",
+                cursor: "pointer",
+                border: "1px solid black",
+              }}
+            ></div>
+            <div
+              className="bg-dark rounded mx-2"
+              onClick={() => {
+                props.toggleMode("dark");
+              }}
+              style={{
+                height: "30px",
+                width: "30px",
+                cursor: "pointer",
+                border: "1px solid black",
+              }}
+            ></div>
+          </div> */}
           {/* <form className="d-flex" role="search">
           <input
             className="form-control me-2"
@@ -46,10 +120,16 @@ export default function Navbar(props) {
             Search
           </button>
         </form> */}
-          <div className={`form-check form-switch text-${props.mode === 'light' ? 'dark' : 'light'}`}>
+          <div
+            className={`form-check form-switch text-${
+              props.mode === "light" ? "dark" : "light"
+            }`}
+          >
             <input
               className="form-check-input"
-              onClick={props.toggleMode}
+              onClick={() => {
+                props.toggleMode(null);
+              }}
               type="checkbox"
               role="switch"
               id="flexSwitchCheckDefault"
@@ -58,7 +138,7 @@ export default function Navbar(props) {
               className="form-check-label"
               htmlFor="flexSwitchCheckDefault"
             >
-              Enable Dark Mode
+              Toggle Mode
             </label>
           </div>
         </div>
--- textutils/src/components/TextForm.js
+++ textutils/src/components/TextForm.js
@@ -18,10 +18,10 @@ export default function TextForm(props) {
     props.showAlert("Text cleared!!!", "success");
   };
   const handleCopy = () => {
-    let text = document.getElementById("myBox");
-    text.select();
-    navigator.clipboard.writeText(text.value);
-    document.getSelection().removeAllRanges();
+    // let text = document.getElementById("myBox");
+    // text.select();
+    navigator.clipboard.writeText(text);
+    // document.getSelection().removeAllRanges();
     props.showAlert("Copied to clipboard!!!", "success");
   };
   const handleExtraSpaces = () => {
@@ -66,7 +66,7 @@ export default function TextForm(props) {
       <div className="container my-3" style={{color: props.mode === 'dark' ? 'white' : '#042743'}}>
         <h2>Your text summary</h2>
         <p>
-          {text.split(" ").filter((element) => {return element.length !== 0}).length} words and {text.length} characters
+          {text.split(/\s+/).filter((element) => {return element.length !== 0}).length} words and {text.length} characters
         </p>
         <p>{0.008 * text.split(" ").filter((element) => {return element.length !== 0}).length} Minutes read</p>
         <h2>Preview</h2>
